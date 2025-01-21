d223098e2db417baaf14598b3cbab588e8aa0d0b
Adarsh Kumar <adarshff06@gmail.com>
2025-01-22 00:34:01 +0530

    Complete Ride Pop up

--- src/components/CaptainDetails.jsx
+++ src/components/CaptainDetails.jsx
@@ -1,49 +1,59 @@
-import React from 'react'
+import React from "react";
+import RidePop from "./RidePop";
 
 function CaptainDetails() {
   return (
     <div className="w-full">
-
-          <div className="w-full flex items-center justify-between">
-
-            <div className="flex w-[52%] items-center justify-between">
-              <div className="w-14 rounded-full overflow-hidden">
-                <img className="w-full h-full object-cover" src="src/assets/Driver_logo.jpeg" alt="" />
-              </div>
-              <div>
-                <h2 className="font-semibold text-lg tracking-tighter">Adarsh Kumar</h2>
-                <p className="text-sm text-gray-400 font-medium -mt-1">Basic Level</p>
-              </div>
-            </div>
-            <div>
-              <p className="text-xl font-semibold">₹ 174.00</p>
-            </div>
-
+      <div className="w-full flex items-center justify-between">
+        <div className="flex w-[52%] items-center justify-between">
+          <div className="w-14 rounded-full overflow-hidden">
+            <img
+              className="w-full h-full object-cover"
+              src="src/assets/Driver_logo.jpeg"
+              alt=""
+            />
           </div>
-
-          <div className="flex w-full items-center justify-between bg-yellow-300 rounded-lg py-4 px-5 mt-6">
-
-            <div className="flex items-center flex-col justify-center">
-              <i class="text-2xl font-medium text-yellow-700 ri-time-line"></i>
-              <h3 className="font-bold">10.2</h3>
-              <p className="text-xs text-yellow-700 font-semibold">Hours Online</p>
-            </div>
-
-            <div className="flex items-center flex-col justify-center">
-              <i class="text-2xl font-medium text-yellow-700 ri-speed-up-fill"></i>
-              <h3 className="font-bold">30 km</h3>
-              <p className="text-xs text-yellow-700 font-semibold">Total Distance</p>
-            </div>
-
-            <div className="flex items-center flex-col justify-center">
-              <i class="text-2xl font-medium text-yellow-700 ri-booklet-line"></i>
-              <h3 className="font-bold">Note</h3>
-              <p className="text-xs text-yellow-700 font-semibold">Notes</p>
-            </div>
+          <div>
+            <h2 className="font-semibold text-lg tracking-tighter">
+              Adarsh Kumar
+            </h2>
+            <p className="text-sm text-gray-400 font-medium -mt-1">
+              Basic Level
+            </p>
           </div>
-
         </div>
-  )
+        <div>
+          <p className="text-xl font-semibold">₹ 174.00</p>
+        </div>
+      </div>
+
+      <div className="flex w-full items-center justify-between bg-yellow-300 rounded-lg py-4 px-5 mt-6">
+        <div className="flex items-center flex-col justify-center">
+          <i class="text-2xl font-medium text-yellow-700 ri-time-line"></i>
+          <h3 className="font-bold">10.2</h3>
+          <p className="text-xs text-yellow-700 font-semibold">Hours Online</p>
+        </div>
+
+        <div className="flex items-center flex-col justify-center">
+          <i class="text-2xl font-medium text-yellow-700 ri-speed-up-fill"></i>
+          <h3 className="font-bold">30 km</h3>
+          <p className="text-xs text-yellow-700 font-semibold">
+            Total Distance
+          </p>
+        </div>
+
+        <div className="flex items-center flex-col justify-center">
+          <i class="text-2xl font-medium text-yellow-700 ri-booklet-line"></i>
+          <h3 className="font-bold">Note</h3>
+          <p className="text-xs text-yellow-700 font-semibold">Notes</p>
+        </div>
+      </div>
+
+      <div className="fixed z-10 bottom-0 left-0  w-full p-5 bg-white">
+        <RidePop />
+      </div>
+    </div>
+  );
 }
 
-export default CaptainDetails
+export default CaptainDetails;
--- src/components/RidePop.jsx
+++ src/components/RidePop.jsx
@@ -2,7 +2,63 @@ import React from 'react'
 
 function RidePop() {
   return (
-    <div>RidePop</div>
+    <div className=''>
+        <div className="flex w-full justify-between items-center px-3">
+            <h2 className="font-bold text-2xl tracking-tight">
+              New Ride Availale.
+            </h2>
+            <div className="text-2xl font-bold"><i className="ri-arrow-down-s-line"></i></div>
+
+         </div>
+         
+         <div className='w-full gap-1 mt-3 flex flex-col items-center justify-center py-4'>
+
+          <div className='w-full px-2 flex items-center justify-between'>
+
+            <div className='w-[60%] flex items-center justify-around'>
+              <div className='w-12 rounded-full overflow-hidden'>
+                <img className='w-full h-full object-cover' src="src/assets/userImage_uber.jpg" alt="" />
+              </div>
+              <h3 className='text-xl font-medium tracking-tight'>Adarsh Kumar</h3>
+            </div>
+            <h3 className='text-xl font-semibold tracking-tight'>2.33 KM</h3>
+
+          </div>
+
+            <div className='w-full flex flex-col gap-1 mt-1'>
+                <div className='flex items-center border-b-2 border-b-gray-400 py-3 w-full gap-4'>
+                    <i className="text-lg font-semibold ri-map-pin-fill"></i>
+                    <div>
+                        <h3 className='text-lg font-medium'>562/11-A</h3>
+                        <p className='text-sm text-gray-600 -mt-1'>Kankaria talab , Ahemdabad</p>
+                    </div>
+                </div>
+
+                <div className='flex items-center w-full gap-4 border-b-2 border-b-gray-400 py-3'>
+                    <i className="text-lg font-semibold ri-map-pin-fill"></i>
+                    <div>
+                        <h3 className='text-lg font-medium'>562/11-A</h3>
+                        <p className='text-sm text-gray-600 -mt-1'>Kankaria talab , Ahemdabad</p>
+                    </div>
+                </div>
+
+                <div className='flex items-center w-full gap-4 border-b-2 border-b-gray-400 py-3'>
+                    <i className="text-lg font-semibold ri-wallet-3-fill"></i>
+                    <div>
+                        <h3 className='text-lg font-medium'>₹ 199.00</h3>
+                        <p className='text-sm text-gray-600 -mt-1'>Cash Cash</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className='w-full mt-7 flex gap-2'>
+                <button className='w-full bg-green-400 text-white font-semibold p-1 rounded-lg'>Confirm</button>
+                <button className='w-full bg-gray-400 text-white font-semibold p-1 rounded-lg'>Ignore</button>
+            </div>
+         </div>
+
+    </div>
   )
 }
 
